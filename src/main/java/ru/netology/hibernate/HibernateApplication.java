07b9202a4920183d0445fe1ea89dcfa85d764574
SokolovNetology <sokolov180375@mail.ru>
2024-08-24 16:20:47 +0300

    hibernate

--- src/main/java/ru/netology/hibernate/HibernateApplication.java
+++ src/main/java/ru/netology/hibernate/HibernateApplication.java
@@ -1,9 +1,11 @@
 package ru.netology.hibernate;
 
+import jakarta.transaction.Transactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Transactional
 public class HibernateApplication {
 
     public static void main(String[] args) {
