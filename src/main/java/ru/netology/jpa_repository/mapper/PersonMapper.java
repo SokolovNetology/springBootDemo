01ceeac60b8865d6470367762d80aa8deb8b43dd
SokolovNetology <sokolov180375@mail.ru>
2024-09-03 14:15:14 +0300

    jpa-repositiry

--- src/main/java/ru/netology/jpa_repository/mapper/PersonMapper.java
+++ src/main/java/ru/netology/jpa_repository/mapper/PersonMapper.java
@@ -1,8 +1,6 @@
 package ru.netology.jpa_repository.mapper;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.netology.jpa_repository.dto.PersonDTO;
 import ru.netology.jpa_repository.entity.Person;
